5df20c8c3dfa695807dfb4c08ccccab25aec2741
oguzyildirim <oguzhanyildirim@protonmail.com>
2021-03-15 20:10:22 +0300

    more exercises

--- rna-transcription/rna_transcription.go
+++ rna-transcription/rna_transcription.go
@@ -1,4 +1,14 @@
+// Package strand provides dna to rna conversion
 package strand
 
-func ToRNA(dna string) string {
+import "strings"
+
+// ToRNA given a DNA strand, return its RNA complement
+func ToRNA(input string) string {
+	return strings.NewReplacer(
+		"G", "C",
+		"C", "G",
+		"T", "A",
+		"A", "U",
+	).Replace(input)
 }
