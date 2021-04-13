286693a91be9b51dde639c578c91723456e5ba0f
oguzyildirim <oguzhanyildirim@protonmail.com>
2021-04-13 19:33:20 +0300

    ...exercises

--- difference-of-squares/difference_of_squares.go
+++ difference-of-squares/difference_of_squares.go
@@ -4,20 +4,14 @@ package diffsquares
 // SquareOfSum calculates square of the sum
 func SquareOfSum(n int) int {
 	var result int
-	sum := 0
-	for i := 0; i <= n; i++ {
-		sum += i
-	}
+	sum := (n * (n + 1)) / 2
 	result = sum * sum
 	return result
 }
 
 // SumOfSquares calculates sum of the squares
 func SumOfSquares(n int) int {
-	var result int
-	for i := 0; i <= n; i++ {
-		result += i * i
-	}
+	result := (n * (n + 1) * (2*n + 1)) / 6
 	return result
 }
 
